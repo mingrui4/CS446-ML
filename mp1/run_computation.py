0e8792a0fe5053acb863905a2eaef387e3b715e0
mingrui4 <mingrui4@illinois.edu>
2018-04-23 14:50:55 -0500

    Add files via upload

---
+++ mp1/run_computation.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+
+def run_computation(inp):
+    '''
+    This function takes all of the steps necessary to run the provided
+    tensor, inp. More specifically:
+        -First, create a session
+        -Next, initialize variables
+        -Then, evaluate the tensor
+        -Finally, return the evaluated value
+
+    Args:
+       inp(tf.Tensor): an arbitrary Tensor to be evaluated
+    Returns:
+       The result of the computation (exact type depends on input)
+
+    '''
+    # Input your code here
+    # inp == True or False
+    sess = tf.Session()
+    initial = tf.global_variables_initializer()
+    sess.run(initial)
+    result = sess.run(inp)
+    #print(result)
+    sess.close()
+    return result
